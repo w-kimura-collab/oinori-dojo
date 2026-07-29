8fcccc01303453457cf0a7a4b440970d199d98de
w-kimura-collab <w-kimura@pftech.co.jp>
2026-07-29 23:09:56 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE = 'dojo-v2';
+const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+    const copy = res.clone();
+    caches.open(CACHE).then(c => c.put(e.request, copy)).catch(()=>{});
+    return res;
+  }).catch(() => caches.match('./index.html'))));
+});
