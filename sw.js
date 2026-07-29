d4f103e495cc8a188d41b959ddc4eba5444287a8
w-kimura-collab <w-kimura@pftech.co.jp>
2026-07-29 23:43:47 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'dojo-v2';
+const CACHE = 'dojo-v10';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
